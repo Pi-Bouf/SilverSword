d0facfc09b7df91bf4248d27241171d46ca4b66b
Pitt05 <pierre.bouffier05@gmail.com>
2016-08-31 22:19:47 +0200

    Pushed only needed files.

---
+++ Game/Game.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SDL.h>
+#include <GL/glew.h>
+#include "Shader.h"
+using namespace std;
+
+class Game
+{
+
+public:
+	Game(string windowTitle, int width, int height);
+	void initGame();
+	void execGame();
+	void exitGame();
+
+private:
+	string windowTitle;
+	int windowWidth;
+	int windowHeight;
+	SDL_Window *gameWindow;
+	SDL_GLContext glContext;
+	SDL_Event sdlEvent;
+	bool endingGame;
+};
+
